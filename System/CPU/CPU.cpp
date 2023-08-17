9ab3f31541335672a81c707661c66dc880320316
Leon <leonvogt2001@gmail.com>
2023-08-17 23:45:13 +0200

    add opcode handling

--- System/CPU/CPU.cpp
+++ System/CPU/CPU.cpp
@@ -3,3 +3,97 @@
 //
 
 #include "CPU.h"
+
+CPU::CPU() {
+    currentOpcode = 0;
+    reg = new Register();
+    memory = new Memory();
+    opcodeTable = {
+            {0x01, std::bind(&CPU::handleGroupOneInstructions, this)}
+    };
+}
+CPU::CPU(Memory *memory) {
+    currentOpcode = 0;
+    reg = new Register();
+    this->memory = memory;
+}
+
+CPU::~CPU(){
+    delete reg;
+}
+
+void CPU::cpuStep() {
+    currentOpcode = readNextByte();
+    parseOpcode();
+}
+
+void CPU::parseOpcode() {
+    uint8_t cc = (currentOpcode & 0x03);
+    switch (cc) {
+        case 0x00:
+            break;
+        case 0x01:
+            handleGroupOneInstructions();
+            break;
+        case 0x02:
+            break;
+        case 0x03:
+            break;
+        default:
+            break;
+    }
+}
+
+void CPU::handleGroupOneInstructions() {
+    uint8_t aaa = (currentOpcode & 0xE0) >> 5;
+    uint8_t bbb = (currentOpcode & 0x1C) >> 2;
+
+    switch(aaa){
+        case 0x00:
+            ORA(bbb);
+            break;
+        case 0x01:
+            AND(bbb);
+            break;
+        case 0x02:
+            EOR(bbb);
+            break;
+        case 0x03:
+            ADC(bbb);
+            break;
+        case 0x04:
+            STA(bbb);
+            break;
+        case 0x05:
+            LDA(bbb);
+            break;
+        case 0x06:
+            CMP(bbb);
+            break;
+        case 0x07:
+            SBC(bbb);
+            break;
+        default:
+            break;
+    }
+}
+
+uint8_t CPU::readNextByte() {
+    uint8_t byte = memory->readByte(reg->PC);
+    reg->PC++;
+    return byte;
+}
+
+uint16_t CPU::readNextWord() {
+    uint8_t lowByte = readNextByte();
+    uint8_t highByte = readNextByte();
+    return static_cast<uint16_t>((highByte << 8) | lowByte);
+}
+
+uint16_t CPU::indirectAddressing(uint8_t value, uint8_t reg) {
+    uint16_t indirectAddress = (memory->readByte(value + reg)
+            + (memory->readByte(value + reg + 1) << 8));
+    return 0;
+}
+
+
--- System/CPU/CPU.h
+++ System/CPU/CPU.h
@@ -5,17 +5,47 @@
 #ifndef INC_6502_EMULATOR_CPU_H
 #define INC_6502_EMULATOR_CPU_H
 
-
-#include <memory>
+#include <unordered_map>
+#include <functional>
 #include "Register.h"
+#include "../Memory/Memory.h"
 
 class CPU {
-private:
-    std::unique_ptr<Register> register;
-
 public:
     CPU();
+    explicit CPU(Memory* memory);
+
     ~CPU();
+
+    void cpuStep();
+    void parseOpcode();
+
+
+private:
+    typedef std::function<void()> InstructionFunction;
+
+    Register* reg;
+    Memory* memory;
+
+    uint8_t currentOpcode;
+    std::unordered_map<uint8_t, InstructionFunction> opcodeTable;
+
+    uint8_t readNextByte();
+    uint16_t readNextWord();
+
+    uint16_t indirectAddressing(uint8_t value, uint8_t reg);
+
+    void handleGroupOneInstructions();
+
+    void ORA(uint8_t bbb);
+    void AND(uint8_t bbb);
+    void EOR(uint8_t bbb);
+    void ADC(uint8_t bbb);
+    void STA(uint8_t bbb);
+    void LDA(uint8_t bbb);
+    void CMP(uint8_t bbb);
+    void SBC(uint8_t bbb);
+
 };
 
 
