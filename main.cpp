4023a7158388337bd52b2918020acab47de5d434
Leon <leonvogt2001@gmail.com>
2023-08-19 14:54:09 +0200

    debug

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ int main() {
     Memory memory;
     CPU cpu(&memory);
 
-    memory.writeByte(0x09, 0x00);
+    memory.writeByte(0x09, 0x0000);
+    memory.writeByte(0xFF, 0x0001);
     //memory.writeByte(0xFF, 0x00FF);
 
    // memory.writeByte(0xA5, 0x0000);
