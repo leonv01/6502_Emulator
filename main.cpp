b0a90a5ef732149692ff4a40d56f909c3822ef31
Leon <leonvogt2001@gmail.com>
2023-08-20 02:24:04 +0200

    debug

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
 #include "System/CPU/CPU.h"
 
 int main() {
     Memory memory;
     CPU cpu(&memory);
 
-    memory.writeByte(0x09, 0x0000);
+    // Load accumulator with zero
+    memory.writeByte(0xA9, 0x0000);
     memory.writeByte(0xFF, 0x0001);
-    //memory.writeByte(0xFF, 0x00FF);
 
-   // memory.writeByte(0xA5, 0x0000);
-   // memory.writeByte(0xFF, 0x0001);
-    cpu.cpuStep();
+    memory.writeByte(0x30, 0x0002);
+    memory.writeByte(0x02, 0x0003);
+
+    cpu.cpuRun();
     cpu.printStatus();
 }
